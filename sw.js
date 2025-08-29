ee68227bf912c7cc0d9fb551a65131927e54fea0
Benson0418 <dges030712@gmail.com>
2025-08-30 02:11:22 +0800

    Site updated: 2025-08-30 02:11:22

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1756489822514";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1756491031415";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
